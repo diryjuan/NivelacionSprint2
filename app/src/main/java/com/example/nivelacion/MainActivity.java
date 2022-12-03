66cdd06db79b0070d8d50fcb9c77aa3cfbfa3703
Juan Diego Henao <eldiryjuan@gmail.com>
2022-12-03 17:42:24 -0500

    Sprint2

--- app/src/main/java/com/example/nivelacion/MainActivity.java
+++ app/src/main/java/com/example/nivelacion/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.nivelacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTextviewRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextviewRegistro=findViewById(R.id.TextviewRegistro);
+        mTextviewRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Registro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
